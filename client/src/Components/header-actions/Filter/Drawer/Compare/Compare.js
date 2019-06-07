6646c530990ba46c723b5adb1306722093884dbc
MMangus <mjmangus16@yahoo.com>
2019-06-07 09:32:14 -0400

    added some bottom padding to compare section

--- client/src/Components/header-actions/Filter/Drawer/Compare/Compare.js
+++ client/src/Components/header-actions/Filter/Drawer/Compare/Compare.js
@@ -5,7 +5,7 @@ import CompareInfo from "./CompareInfo";
 
 const Compare = ({ discs, removeCompare, toggleRatingsStatus }) => {
   return (
-    <div style={{ width: "100%", height: "auto" }}>
+    <div style={{ width: "100%", height: "auto", paddingBottom: 25 }}>
       {discs.length > 0 ? (
         discs.map((disc, i) => (
           <Disc
